c1931025a560e28e9a485369ff9acee68c900581
smellybandit911 <smellybandit911@gmail.com>
2026-06-30 13:45:03 -0400

    Update players-dpll.ts

--- src/data/players-dpll.ts
+++ src/data/players-dpll.ts
@@ -5,7 +5,9 @@
 import { Player } from './players';
 
 const dpllPlayers: Player[] = [
-  { rank: 1, name: "CeasarWare", points: 200, hardestLevel: "free DAVID" },
+  { rank: 1, name: "CeasarWare", points: 703, hardestLevel: "free DAVID" },
+  { rank: 2, name: "The cats", points: 167.016, hardestLevel: "Davids baconator meal" },
+  { rank: 3, name: "David", points: 167, hardestLevel: "Davids baconator meal" },
 ];
 
 export default dpllPlayers;
