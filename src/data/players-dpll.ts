c9e124a7be43b07f8c19391506bee6e9913c2bc8
smellybandit911 <smellybandit911@gmail.com>
2026-07-11 00:14:50 -0400

    Updated players.ts

--- src/data/players-dpll.ts
+++ src/data/players-dpll.ts
@@ -5,9 +5,7 @@
 import { Player } from './players';
 
 const dpllPlayers: Player[] = [
-  { rank: 1, name: "CeasarWare", points: 703, hardestLevel: "free DAVID" },
-  { rank: 2, name: "The cats", points: 167.016, hardestLevel: "Davids baconator meal" },
-  { rank: 3, name: "David", points: 167, hardestLevel: "Davids baconator meal" },
+  { rank: 1, name: "CeasarWare", points: 200, hardestLevel: "free DAVID" },
 ];
 
 export default dpllPlayers;
--- src/data/players.ts
+++ src/data/players.ts
@@ -16,18 +16,17 @@ export interface Player {
 }
 
 const players: Player[] = [
-    { rank: 1, name: "CeasarWare", points: 3012, hardestLevel: "Davids inferno rebirth" },
-  { rank: 2, name: "vvKyro", points: 2052, hardestLevel: "elementary for dxvid" },
-  { rank: 3, name: "SmellY", points: 1787, hardestLevel: "David x lucas circles" },
-  { rank: 4, name: "icedagger!!", points: 1461, hardestLevel: "elementary for david" },
-  { rank: 5, name: "vvKyro", points: 2052, hardestLevel: "elementary for dxvid" },
-  { rank: 6, name: "David", points: 1267, hardestLevel: "davids goopium syrup" },
-  { rank: 7, name: "limboproductions", points: 1267, hardestLevel: "haunted david" },
-  { rank: 8, name: "ShayPlazYT", points: 375, hardestLevel: "DavidFemHouse" },
-  { rank: 9, name: "randomperson", points: 333, hardestLevel: "David rage stage" },
-  { rank: 10, name: "chudlas", points: 98, hardestLevel: "Davids retray wave" },
-  { rank: 11, name: "sirius", points: 0 },
-  { rank: 12, name: "ratafak", points: 67, hardestLevel: "thinking space 2" },
+  { rank: 1, name: "CeasarWare", points: 2758, hardestLevel: "Davids inferno" },
+  { rank: 2, name: "SmellY", points: 1787, hardestLevel: "David x lucas circles" },
+  { rank: 3, name: "icedagger!!", points: 1461, hardestLevel: "elementary for david" },
+  { rank: 4, name: "vvKyro", points: 1461, hardestLevel: "elementary for dxvid" },
+  { rank: 5, name: "David", points: 1267, hardestLevel: "davids goopium syrup" },
+  { rank: 6, name: "limboproductions", points: 1267, hardestLevel: "haunted david" },
+  { rank: 7, name: "ShayPlazYT", points: 375, hardestLevel: "DavidFemHouse" },
+  { rank: 8, name: "randomperson", points: 333, hardestLevel: "David rage stage" },
+  { rank: 9, name: "chudlas", points: 0, hardestLevel: "Davids retray wave" },
+  { rank: 10, name: "sirius", points: 0 },
+  { rank: 11, name: "ratafak", points: 67, hardestLevel: "thinking space 2" },
 ];
 
 export default players;
